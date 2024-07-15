ce8869cd910bc4bb4113149e4901146dc5680ec6
Akyanna <durkina.a.n@yandex.ru>
2024-07-15 16:12:45 +0300

    Вносит правки по проверке линтера

--- js/functions.js
+++ js/functions.js
@@ -19,8 +19,8 @@ function checkTimeMeeting (startDay, endDay, startMeeting, durationMeeting) {
 '14:00' - начало встречи
 90 - продолжительность встречи в минутах
 */
-console.log(checkTimeMeeting('08:00', '17:30', '14:00', 90)); // true
-console.log(checkTimeMeeting('8:0', '10:0', '8:0', 120)); // true
-console.log(checkTimeMeeting('08:00', '14:30', '14:00', 90)); // false
-console.log(checkTimeMeeting('14:00', '17:30', '08:0', 90)); // false
-console.log(checkTimeMeeting('8:00', '17:30', '08:00', 900)); // false
+checkTimeMeeting('08:00', '17:30', '14:00', 90); // true
+checkTimeMeeting('8:0', '10:0', '8:0', 120); // true
+checkTimeMeeting('08:00', '14:30', '14:00', 90); // false
+checkTimeMeeting('14:00', '17:30', '08:0', 90); // false
+checkTimeMeeting('8:00', '17:30', '08:00', 900); // false

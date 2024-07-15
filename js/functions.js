226d68cd57d7d852e0f9380e4693671eeffcaada
Akyanna <durkina.a.n@yandex.ru>
2024-07-15 16:11:01 +0300

    Выполняет задание 5.16

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,26 @@
-const getLengthString = (string, maxLength) => {
-  if (string.length <= maxLength) {
-    return true;
-  }
-  return false;
+const getTimeMinutes = function (time) {
+  const [hour, minutes] = time.split(':');
+  const timeInMinutes = 60;
+
+  return hour * timeInMinutes + parseInt(minutes, 10);
 };
 
-getLengthString('Hello,JavaScript!', 2);
+function checkTimeMeeting (startDay, endDay, startMeeting, durationMeeting) {
+  const startDayPerMinutes = getTimeMinutes(startDay);
+  const endDayPerMinutes = getTimeMinutes(endDay);
+  const startMeetingPerMinutes = getTimeMinutes(startMeeting);
 
+  return startMeetingPerMinutes >= startDayPerMinutes && startMeetingPerMinutes + durationMeeting <= endDayPerMinutes;
+}
 
-const palindrom = (string) => {
-  const checkString = '';
-  let trimString = checkString.toLowerCase().replaceAll();
-  for (let i = trimString.length - 1; i >= 0; i--) {
-    trimString += string[i];
-  }
-  return trimString === checkString;
-};
-
-palindrom('Лёша на полке клопа нашёл ');
-
+/*
+'8:00' - начало рабочего дня
+'17:30' - конец рабочего дня
+'14:00' - начало встречи
+90 - продолжительность встречи в минутах
+*/
+console.log(checkTimeMeeting('08:00', '17:30', '14:00', 90)); // true
+console.log(checkTimeMeeting('8:0', '10:0', '8:0', 120)); // true
+console.log(checkTimeMeeting('08:00', '14:30', '14:00', 90)); // false
+console.log(checkTimeMeeting('14:00', '17:30', '08:0', 90)); // false
+console.log(checkTimeMeeting('8:00', '17:30', '08:00', 900)); // false

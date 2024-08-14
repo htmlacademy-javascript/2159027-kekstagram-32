ed4a58418eb5f819affa706d9d9ad88a43532f00
Akyanna <durkina.a.n@yandex.ru>
2024-08-14 13:39:48 -0700

    Правки по результатам проверки

--- js/util.js
+++ js/util.js
@@ -3,25 +3,6 @@ const TIMEOUT_DELAY = 500;
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-const getRandomPositiveInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-
-  return Math.floor(result);
-};
-
-const getRandomArrayElement = (elements) => {
-  const randomIndex = getRandomPositiveInteger(0, elements.length - 1);
-  return elements[randomIndex];
-};
-
-const getCommentIdGenerator = () => {
-  let commentId = 1;
-
-  return () => commentId++;
-};
-
 const dataErrorTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
 
 const showAlert = (message) => {
@@ -34,7 +15,6 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-
 function debounce(callback, timeoutDelay = TIMEOUT_DELAY) {
   let timeoutId;
   return (...rest) => {
@@ -43,11 +23,7 @@ function debounce(callback, timeoutDelay = TIMEOUT_DELAY) {
   };
 }
 
-
 export {
-  getRandomArrayElement,
-  getCommentIdGenerator,
-  getRandomPositiveInteger,
   isEscapeKey,
   showAlert,
   debounce
